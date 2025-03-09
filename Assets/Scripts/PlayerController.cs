b8355fdc6f23cd9c793a070c46c8bc1e0038ae4c
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-03-09 18:28:43 +0000

    Enemy Character and 2D Sprite Animations Implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,17 +20,32 @@ public class PlayerController : MonoBehaviour
     public float timeInvincible = 0.5f;
     bool isInvincible;
     float damageCooldown;
+
+    Animator animator;
+ Vector2 moveDirection = new Vector2(1,0);
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+      if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f))
+     {
+      moveDirection.Set(move.x, move.y);
+        moveDirection.Normalize();
+
+     }
+     animator.SetFloat("Look X", moveDirection.x);
+    animator.SetFloat("Look Y", moveDirection.y);
+    animator.SetFloat("Speed", move.magnitude);
+
+
         move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
 
@@ -60,6 +75,7 @@ public class PlayerController : MonoBehaviour
    }
    isInvincible = true;
    damageCooldown = timeInvincible;
+   animator.SetTrigger("Hit");
 }
 
     currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
