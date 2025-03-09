374527ba9a8c2eaf659f4d5d3bf1fead9c02d370
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-03-09 19:41:00 +0000

    a60900 Project 6 Attempted script fix (Failed)
    The dialogue won't show ingame. The Debug log text functioned, which makes this strange

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-
 public class UIHandler : MonoBehaviour
 {
-    public static UIHandler instance { get; private set; }
    private VisualElement m_Healthbar;
+   public static UIHandler instance { get; private set; }
 
+   // UI dialogue window variables
    public float displayTime = 4.0f;
-private VisualElement m_NonPlayerDialogue;
-private float m_TimerDisplay;
+   private VisualElement m_NonPlayerDialogue;
+   private float m_TimerDisplay;
 
 
-   // Start is called before the first frame update
+   // Awake is called when the script instance is being loaded (in this situation, when the game scene loads)
    private void Awake()
    {
-      instance = this;
+       instance = this;
    }
 
-   void Start()
+   // Start is called before the first frame update
+   private void Start()
    {
        UIDocument uiDocument = GetComponent<UIDocument>();
        m_Healthbar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBar");
@@ -30,13 +31,15 @@ private float m_TimerDisplay;
        m_NonPlayerDialogue = uiDocument.rootVisualElement.Q<VisualElement>("NPCDialogue");
        m_NonPlayerDialogue.style.display = DisplayStyle.None;
        m_TimerDisplay = -1.0f;
+
+
    }
 
 
-   
+
    private void Update()
    {
-      if (m_TimerDisplay > 0)
+       if (m_TimerDisplay > 0)
        {
            m_TimerDisplay -= Time.deltaTime;
            if (m_TimerDisplay < 0)
@@ -48,11 +51,13 @@ private float m_TimerDisplay;
        }
    }
 
+
    public void SetHealthValue(float percentage)
    {
        m_Healthbar.style.width = Length.Percent(100 * percentage);
    }
-
+   
+   
    public void DisplayDialogue()
    {
    m_NonPlayerDialogue.style.display = DisplayStyle.Flex;
