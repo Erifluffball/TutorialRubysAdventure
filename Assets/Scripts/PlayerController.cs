c8440f44a5db73a3456e77ff102f85f100fb9b12
Erifluffball <101990800+Erifluffball@users.noreply.github.com>
2025-03-09 16:11:49 +0000

    Basic Health System and Collectible
    First three steps of the Health System part

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; }}
+    int currentHealth = 1;
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        //currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -26,6 +32,12 @@ public class NewBehaviourScript : MonoBehaviour
   {
     Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
     rigidbody2d.MovePosition(position);
+  }
+
+  public void ChangeHealth (int amount)
+  {
+    currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    Debug.Log(currentHealth + "/" + maxHealth);
   }
 
 }
